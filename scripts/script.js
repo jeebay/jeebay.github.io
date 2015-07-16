06bf377265299d8c7cab076c9584eafc71129e71
Brian Johnson <brian@Brians-MacBook-Pro.local>
2015-07-16 16:19:58 -0400

    refactored card stack constructor, deleted value calculator and made a standalone function to determine the blackjack value. added blackjack detection on initial deal

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 console.log("LINKED");
-// $(document).ready(function(){
+$(document).ready(function(){
 
     // Create card prototype with card
     function Card(cardName,value,suit,imageUrl){
@@ -40,29 +40,9 @@ console.log("LINKED");
             }
             return dealtCards;
         };
-
-        // return total value of cards in stack
-        this.totalValue = function(){
-            var cards = this.cards;
-            var totalArray = [];
-            var total = 0;
-            for (var i = 0; i < cards.length; i++) {
-                totalArray.push(cards[i].value);
-                total += totalArray[i];
-            }
-            if (total > 21) {
-                for (var k = 0; k < totalArray.length; k++) {
-                    if(totalArray[k] === 11) {
-                        total -= 10;
-                    }
-                }
-            }
-            return total;
-        };
     }// End CardStack factory
 
     // Creating arrays of file names, card names, suits, and values in order to create a deck of cards
-    var imgFileNames = ['1.png','2.png','3.png','4.png','5.png','6.png','7.png','8.png','9.png','10.png','11.png','12.png','13.png','14.png','15.png','16.png','17.png','18.png','19.png','20.png','21.png','22.png','23.png','24.png','25.png','26.png','27.png','28.png','29.png','30.png','31.png','32.png','33.png','34.png','35.png','36.png','37.png','38.png','39.png','40.png','41.png','42.png','43.png','44.png','45.png','46.png','47.png','48.png','49.png','50.png','51.png','52.png'];
 
     var nameArray = (function(){
         var suits = ['Clubs','Spades','Hearts','Diamonds'];
@@ -89,7 +69,8 @@ console.log("LINKED");
     })();
 
     var valueArray = [11,11,11,11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,8,8,8,8,7,7,7,7,6,6,6,6,5,5,5,5,4,4,4,4,3,3,3,3,2,2,2,2];
-
+    
+    var imgFileNames = ['1.png','2.png','3.png','4.png','5.png','6.png','7.png','8.png','9.png','10.png','11.png','12.png','13.png','14.png','15.png','16.png','17.png','18.png','19.png','20.png','21.png','22.png','23.png','24.png','25.png','26.png','27.png','28.png','29.png','30.png','31.png','32.png','33.png','34.png','35.png','36.png','37.png','38.png','39.png','40.png','41.png','42.png','43.png','44.png','45.png','46.png','47.png','48.png','49.png','50.png','51.png','52.png'];
     var imageUrlArray = (function(fileNames){
         var fullPaths = [];
         var directory = 'images/cards/classic-cards/';
@@ -124,48 +105,80 @@ console.log("LINKED");
         computerHand = new CardStack(playingDeck.dealCards(2));
         $('.player-hand').empty();
         $('.computer-hand').empty();
-        $('.computer-total').text(computerHand.totalValue());
-        $('.player-total').text(playerHand.totalValue());
-        drawHand(playerHand,$playerHand);
-        drawHand(computerHand,$computerHand);
-    }
-
-    function hit (deck,hand){
-        hand.addCard(deck.dealCards(1)[0]);
-        if (hand.totalValue() > 21) {
-            return "Busted with "+hand.totalValue();
-        } else {
-            return hand.totalValue();
+        $('.computer-total').text(blackJackValue(computerHand));
+        $('.player-total').text(blackJackValue(playerHand));
+        playerHand.cards.forEach(function(card){
+            drawHand(card,$playerHand)
+        });
+        computerHand.cards.forEach(function(card){
+            drawHand(card,$computerHand)
+        });
+        if (blackJackValue(playerHand) === 21 && blackJackValue(computerHand === 21)) {
+            gamePush();
+        } else if (blackJackValue(playerHand) === 21) {
+            computerLose();
+        } else if (blackJackValue(computerHand) === 21) {
+            playerLose();
         }
     }
 
-    function autoPlay(hand){
-        while (hand.totalValue() < 17){
-            hit(playingDeck,hand);
-        }
-        if (hand.totalValue() > 21) {
-            return "Busted with "+hand.totalValue();
+    function blackJackValue(deck){
+            var cardsInHand = deck['cards'];
+            var totalArray = [];
+            var total = 0;
+            for (var i = 0; i < cardsInHand.length; i++) {
+                totalArray.push(cardsInHand[i].value);
+                total += totalArray[i];
+            }
+            if (total > 21) {
+                for (var k = 0; k < totalArray.length; k++) {
+                    if(totalArray[k] === 11) {
+                        total -= 10;
+                    }
+                }
+            }
+            return total;
+        };
+
+    function hit (deck,hand,$section){
+        var newCard = deck.dealCards(1)[0];
+        hand.addCard(newCard);
+        drawHand(newCard,$section);
+        $section.next().text(blackJackValue(hand));
+        if (blackJackValue(hand) > 21) {
+            return "Busted with "+blackJackValue(hand);
         } else {
-            return hand.totalValue();
+            return blackJackValue(hand);
+        }
+    }
+
+    function autoPlay(hand,$section){
+        while (blackJackValue(hand) < 17){
+            hit(playingDeck,hand,$section);
+        }
+        if (blackJackValue(hand) > 21) {
+            return "Busted with "+blackJackValue(hand);
+        } else {
+            return blackJackValue(hand);
         }
     }
 
     function playerLose() {
         $('#lose-modal').toggle(); // CREATE lose-modal
         alert("you lose");
-        dealNewHand();
+        // dealNewHand();
     }
 
     function computerLose() {
         $('#win-modal').toggle();   // CREATE win-modal
         alert("you win");
-        dealNewHand();
+        // dealNewHand();
     }
 
     function gamePush() {
         $('#push-modal').toggle();   // CREATE win-modal   
         alert("push! it's a tie!");
-        dealNewHand();
+        // dealNewHand();
     }
 
     function decideWinner(playerTotal,computerTotal) {
@@ -178,12 +191,10 @@ console.log("LINKED");
         }
     }
 
-    function drawHand (deck,$section) {
-        for (var i = 0; i < deck.cards.length; i++) {
-            var $newCard = $('<div>').attr('class','card');
-            $newCard.css('background-image',deck.cards[i].imageUrl);
-            $section.append($newCard);
-        };
+    function drawHand (card,$section) {        
+        var $newCard = $('<div>').attr('class','card animated fadeInDown');
+        $newCard.css('background-image',card.imageUrl);
+        $section.append($newCard);
     }
 
     $('#win-play-again').on('click',function(event){
@@ -197,27 +208,18 @@ console.log("LINKED");
     });
 
     $('.hit').on('click',function(event){
-        var result = hit(playingDeck,playerHand);
+        var result = hit(playingDeck,playerHand,$('.player-hand'));
         if (typeof(result) === "string") {
-            $('.player-total').text(result);
-            $('.player-hand').text(playerHand.cardsInDeck());
             playerLose();
-        } else {
-            $('.player-total').text(result);
-            $('.player-hand').text(playerHand.cardsInDeck());
-        }
+        } 
     });
 
     $('.stand').on('click',function(){
-        var computerResult = autoPlay(computerHand);
-        if (typeof(computerResult) === "string") {
-            $('.computer-total').text(computerResult);
-            $('.computer-hand').text(computerHand.cardsInDeck());
-            computerLose()
+        var computerResult = autoPlay(computerHand,$('.computer-hand'));
+        if (typeof(computerResult) === "string") {            
+            computerLose();
         } else {
-            $('.computer-total').text(computerResult);
-            $('.computer-hand').text(computerHand.cardsInDeck());
-            decideWinner();
+            decideWinner(blackJackValue(playerHand),blackJackValue(computerHand));
         }
     });
 
@@ -225,4 +227,4 @@ console.log("LINKED");
 
     
 
-// })
+})
