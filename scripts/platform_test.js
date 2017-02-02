14a27c910212d10d3c0600e2de69069a0af5332b
Brian Johnson <brian.johnson@mastercard.com>
2017-02-02 13:34:26 -0500

    even less es6

--- scripts/platform_test.js
+++ scripts/platform_test.js
@@ -9,13 +9,13 @@ const browser = root.bowser._detect(ua);
 
 $ua.html(`${window.navigator.userAgent}`);
 
-Object.keys(root.platform).forEach(key => {
+Object.keys(root.platform).forEach(function(key) {
     if (key !== 'parse') {
         $platformOutput.append($('<li><pre>platform.' + key + ' = ' + platform[key] + '</pre></li>'));
     }
 });
 
-Object.keys(browser).forEach(key => {
+Object.keys(browser).forEach(function(key) {
     $bowserOutput.append($('<li><pre> bowser.' + key + ' = ' + browser[key] + '</pre></li>'));
 });
 
