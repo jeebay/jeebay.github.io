467e41b0861ed9f5f64d5b714d43635fa6098063
Brian Johnson <brian.johnson@mastercard.com>
2017-02-02 11:04:07 -0500

    test of platform vs bowser

---
+++ scripts/platform_test.js
@@ -0,0 +1,19 @@
+(function(root) {
+
+const $platformOutput = $('#platform_output');
+const $bowserOutput = $('#bowser_output');
+
+const ua = root.navigator.userAgent;
+const browser = root.bowser._detect(ua);
+
+Object.keys(root.platform).forEach(key => {
+    if (key !== 'parse') {
+        $platformOutput.append($(`<li><pre>platform.${key} = ${platform[key]}</pre></li>`))
+    }
+});
+
+Object.keys(browser).forEach(key => {
+    $bowserOutput.append($(`<li><pre>bowser.${key} = ${browser[key]}</pre></li>`))
+});
+
+})(window);
